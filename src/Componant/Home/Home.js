0b4cfc3665d960b8f941006345bb502ade4c3f0a
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-09-30 19:13:27 +0600

    make website responsive and modify cart

--- src/Componant/Home/Home.js
+++ src/Componant/Home/Home.js
@@ -4,6 +4,7 @@ import './Home.css'
 
 const Home = () => {
     const [activitys, setActivitys] = useState([]);
+    const [data, setData] = useState([]);
     useEffect(() => {
         fetch('activity.json')
             .then(res => res.json())
@@ -13,6 +14,8 @@ const Home = () => {
 
     const handleClick = (activity) => {
         console.log(activity);
+        const newData = [...data, activity]
+        setData(newData);
     }
 
     return (
@@ -30,7 +33,26 @@ const Home = () => {
                 </div>
                 <div className='data-side'>
                     <div className='cart'>
-                        <p>This is player cart</p>
+                        <h2>Shahriyar</h2>
+                        <p>Dhaka, bangladesh</p>
+                        <div className='personal-info'>
+                            <p><b>60</b><small>kg</small> <br /> <small>Weight</small></p>
+                            <p><b>6.5</b><br /><small>Height</small></p>
+                            <p><b>25</b><small>yrs</small> <br />
+                                <small>Age</small></p>
+                        </div>
+                        <h3>Add a break</h3>
+                        <div className='btn-con'>
+                            <button className='break-btn'>10m</button>
+                            <button className='break-btn'>20m</button>
+                            <button className='break-btn'>30m</button>
+                            <button className='break-btn'>40m</button>
+
+                        </div>
+                        <h3>Exercize detile</h3>
+                        <p>Exersize detile:{ }</p>
+                        <p>Break time:{data.length}</p>
+                        <button className='btn-com'>Activity complite</button>
                     </div>
                 </div>
             </div>
