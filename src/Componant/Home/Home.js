b8a1283ae29b67beb72df938ec1e263f6d962234
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-09-30 14:32:00 +0600

    added json

--- src/Componant/Home/Home.js
+++ src/Componant/Home/Home.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 
 const Home = () => {
+    const [activitys, setActivitys] = useState([]);
+    useEffect(() => {
+        fetch('activity.json')
+            .then(res => res.json())
+            .then(data => setActivitys(data))
+
+    }, [])
     return (
         <div>
             <h1>I am from home</h1>
             <div className='home-container'>
-                <div className='left-side'>
-                    <h1>all activity</h1>
+                <div className='active-side'>
+                    <h1>all activity: {activitys.length}</h1>
                 </div>
-                <div className='right-side'>
+                <div className='data-side'>
                     <h1>cart</h1>
+                    <div className='cart'>
+                        <p>This is player cart</p>
+                    </div>
                 </div>
             </div>
         </div>
